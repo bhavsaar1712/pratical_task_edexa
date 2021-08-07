66375fc6e94bbe33d4a7e2db5c8ef8ac57ca3708
bhavsaar1712 <bhavsar17121998@gmail.com>
2021-08-07 23:49:54 +0530

    final push

---
+++ Dump20210807/test_users_auth.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: test
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users_auth`
+--
+
+DROP TABLE IF EXISTS `users_auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users_auth` (
+  `ID` int NOT NULL,
+  `Users` varchar(50) DEFAULT NULL,
+  `Is_insert` int NOT NULL,
+  `Is_update` int NOT NULL,
+  `Is_delete` int NOT NULL,
+  `Is_view` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_auth`
+--
+
+LOCK TABLES `users_auth` WRITE;
+/*!40000 ALTER TABLE `users_auth` DISABLE KEYS */;
+INSERT INTO `users_auth` VALUES (1001,'HR',0,1,1,1),(1002,'Manager',1,1,1,1),(1003,'team lead',0,0,0,0),(1004,'employees',0,0,0,0);
+/*!40000 ALTER TABLE `users_auth` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-07 23:44:07
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # pratical_task_edexa
+# in this project i had also include database kindly import it 
+# also make the changes in config file as per as your data base 
+
+# host     : 'localhost',
+#   user     : 'root',
+#    password : 'password',
+#   database : 'test',
+ #   port :'3306',
+  #  nodeport:'8000',
+   
+# thnak you 
